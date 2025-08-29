a7cb434bd7c942ceaec2b0612bee02f359c469e6
vincenzo.mancuso <vincenzo.mancuso@adevinta.com>
2025-08-29 15:54:27 +0200

    add Title component and utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Button } from "./stories/Button"
+import { Button } from "./stories/Button/Button";
+import Title from "./stories/Headings/Title";
 
 function App() {
- return (
-  <div>
-    <Button label="Update"/>
-  </div>
- )
+  return (
+    <div>
+      <p className="text-red-600">Hello</p>
+      <Button label="Update" />
+      <Title className=" bg-amber-300">Hello</Title>
+    </div>
+  );
 }
 
-export default App
+export default App;
