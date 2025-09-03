e1e85df909d7d05ef1aa970e8a9f96ce1df35697
vincenzo.mancuso <vincenzo.mancuso@adevinta.com>
2025-09-03 17:46:46 +0200

    edit

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,3 @@
-import "./index.css";
-
+// This file is for generating TypeScript declarations only
 export { Button, type ButtonProps } from "./components/Button/Button";
 export { Heading, type HeadingProps } from "./components/Heading/Heading";
